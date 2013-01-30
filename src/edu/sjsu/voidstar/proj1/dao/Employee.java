d417e65f354f25322e0c208df20e1df2fc54f574
Shaun Guth <shaun.guth@gmail.com>
2013-01-29 19:59:42 -0800

    Setting up maven integration for hibernate

--- src/edu/sjsu/voidstar/proj1/dao/Employee.java
+++
@@ -1,8 +0,0 @@
-package edu.sjsu.voidstar.proj1.dao;
-
-import org.hibernate.annotations.Entity;
-
-@Entity
-public class Employee {
-
-}
--- src/edu/sjsu/voidstar/proj1/main/Main.java
+++
@@ -1,8 +0,0 @@
-package edu.sjsu.voidstar.proj1.main;
-
-public class Main {
-
-	public static void main (String... args) { 
-		System.out.println("Hello");
-	}
-}
