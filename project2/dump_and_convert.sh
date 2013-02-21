5fc58af172cbe0bd4c12ab50f0be2c29eb3f0ba0
Jason <jason@jason-Lenovo-IdeaPad-Y570.(none)>
2013-02-20 18:15:09 -0800

    Deleting duplicate xml/xsd and scripts

--- project2/dump_and_convert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for table in City Country CountryLanguage Infection Language; do
-    echo "Dumping $table into $table.xml"
-    mysqldump -ushaun -pnuahs se157bproject1 --xml $table | xsltproc mysql.xsl - > $table.xml
-done;
--- project2/validate_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for table in City Country CountryLanguage Infection Language; do
-    xmllint --noout --schema $table.xsd $table.xml
-done;
