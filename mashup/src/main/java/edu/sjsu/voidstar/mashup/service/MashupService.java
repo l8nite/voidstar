aba0d754a7e2e793545357511a6b182cc70f6130
Shaun Guth <shaun.guth@gmail.com>
2013-03-10 20:01:36 -0700

    some other mashup templates

--- mashup/src/main/java/edu/sjsu/voidstar/mashup/service/MashupService.java
+++ mashup/src/main/java/edu/sjsu/voidstar/mashup/service/MashupService.java
@@ -1,5 +1,7 @@
 package edu.sjsu.voidstar.mashup.service;
 
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -7,10 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.sjsu.voidstar.ws.dao.Country;
-import edu.sjsu.voidstar.ws.mashup.MashupPortService;
-import edu.sjsu.voidstar.ws.soap.city.CityPortService;
+import edu.sjsu.voidstar.ws.dao.Language;
 import edu.sjsu.voidstar.ws.soap.country.CountryPortService;
 import edu.sjsu.voidstar.ws.soap.country.CountryService;
+import edu.sjsu.voidstar.ws.soap.language.LanguagePortService;
+import edu.sjsu.voidstar.ws.soap.language.LanguageService;
 
 import javax.xml.ws.WebServiceRef;
 
@@ -19,20 +22,35 @@ public class MashupService {
 	@WebServiceRef(wsdlLocation = "http://localhost:8123/country?wsdl")
 	static CountryPortService countryPortService = new CountryPortService();
 
-	private static final Logger log = LoggerFactory
-			.getLogger(MashupService.class);
+	@WebServiceRef(wsdlLocation = "http://localhost:8123/language?wsdl")
+	static LanguagePortService languagePortService = new LanguagePortService();
+
+	private static final Logger log = LoggerFactory.getLogger(MashupService.class);
 
 	@WebMethod
-	public String getMostSpokenLanguageInCountry(
-			@WebParam(name = "countryCode") String countryCode) {
-		// get country from CountryService
-		CountryService countryService = countryPortService
-				.getCountryServicePort();
+	public String getLanguagesSpokenInCountry(@WebParam(name = "countryCode") String countryCode) {
+		log.info("getLanguagesSpokenInCountry(" + countryCode + ")");
+
+		CountryService countryService = countryPortService.getCountryServicePort();
 		Country country = countryService.getCountryByCode(countryCode);
-		log.info("getMostSpokenLanguageInCountry(" + countryCode + ")");
 
-		// TODO do something with languages
+		LanguageService languageService = languagePortService.getLanguageServicePort();
+		List<Language> languages = languageService.getLanguagesByCountry(country);
 
-		return country.getHeadOfState();
+		return languages.toString();
+	}
+
+	@WebMethod
+	public String getNumberOfZombiesInCity(@WebParam(name = "cityName") String cityName) {
+		// CityService.getCityByName(cityName)
+		// InfectionService.getInfectionByCity(city)
+		log.info("getNumberOfZombiesInCity(" + cityName + ")");
+		return cityName;
+	}
+
+	@WebMethod
+	public String getCountriesWhichSpeakLanguage(@WebParam(name = "languageName") String languageName) {
+		log.info("getCountriesWhichSpeakLanguage(" + languageName + ")");
+		return languageName;
 	}
 }
