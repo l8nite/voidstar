01658a3dc48d2890c1826d2de6ef9504f26e0879
Jason Campos <jcampos8782@gmail.com>
2013-03-13 10:28:47 -0700

    Adding copyright to new files

--- service/src/main/java/us/p/opulo/dao/library/CountryLibrary.java
+++ service/src/main/java/us/p/opulo/dao/library/CountryLibrary.java
@@ -1,3 +1,18 @@
+/**
+ *                               .__
+ * ______      ____ ______  __ __|  |   ____      __ __  ______
+ * \____ \    /  _ \\____ \|  |  \  |  /  _ \    |  |  \/  ___/
+ * |  |_> >  (  <_> )  |_> >  |  /  |_(  <_> )   |  |  /\___ \
+ * |   __/ /\ \____/|   __/|____/|____/\____/ /\ |____//____  >
+ * |__|    \/       |__|                      \/            \/
+ *
+ *
+ * Copyright (c) 2013 Jason Campos <jcampos8782@gmail.com>, Shaun Guth, Ash Islam
+ * All Rights Reserved.
+ *
+ * This software is licensed under The MIT License (MIT)
+ * http://opensource.org/licenses/MIT
+ */
 package us.p.opulo.dao.library;
 
 import java.util.List;
