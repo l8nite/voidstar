aef7eb6a03d1a2bff6b39071904f6c358deda159
Jason Campos <jcampos8782@gmail.com>
2013-03-14 15:18:48 -0700

    Removing unused import

--- mashup/src/main/java/edu/sjsu/voidstar/mashup/service/MashupService.java
+++ mashup/src/main/java/edu/sjsu/voidstar/mashup/service/MashupService.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
