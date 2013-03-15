0f1bab312a2a1d0f49fa81ab7eff011eea6f4597
Shaun Guth <shaun.guth@gmail.com>
2013-03-15 01:56:59 -0700

    wrong argument in wsimport

--- client/bin/wsimports.sh
+++ client/bin/wsimports.sh
@@ -1,1 +1,1 @@
- wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/mashup-0.0.1-SNAPSHOT/mashup?wsdl
+ wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/mashup-0.0.1-SNAPSHOT/mashup?wsdl
--- mashup/bin/wsimports.sh
+++ mashup/bin/wsimports.sh
@@ -1,6 +1,6 @@
- wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/city?wsdl
- wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/country?wsdl
- wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/countrylanguage?wsdl
- wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/infection?wsdl
- wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/language?wsdl
- wsimport -verbose -extension -s src/main/java/ -g target/ http://www.webservicex.net/geoipservice.asmx?WSDL
+ wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/city?wsdl
+ wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/country?wsdl
+ wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/countrylanguage?wsdl
+ wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/infection?wsdl
+ wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/language?wsdl
+ wsimport -verbose -extension -s src/main/java/ -d target/ http://www.webservicex.net/geoipservice.asmx?WSDL
