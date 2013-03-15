3c29804cd5602ad95aa1338cb4bd6a933bf10b24
Shaun Guth <shaun.guth@gmail.com>
2013-03-14 23:32:07 -0700

    run wsimport against triton

---
+++ mashup/bin/wsimports.sh
@@ -0,0 +1,5 @@
+ wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/city?wsdl
+ wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/country?wsdl
+ wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/countrylanguage?wsdl
+ wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/infection?wsdl
+ wsimport -verbose -s src/main/java/ -g target/ http://triton.idlemonkeys.net:8080/service-1.0-SNAPSHOT/language?wsdl
