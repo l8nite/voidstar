24ebfd1fc4ca5f9dde58497eba61a8a40e6fc12c
Shaun Guth <shaun.guth@gmail.com>
2013-03-18 01:52:23 -0700

    Switch to p.opulo.us for wsimports

--- client/bin/wsimports.bat
+++ client/bin/wsimports.bat
@@ -1,1 +1,1 @@
- wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/mashup-1.0/mashup?wsdl
+ wsimport -verbose -s src/main/java/ -d target/ http://p.opulo.us:8080/mashup-1.0/mashup?wsdl
--- mashup/bin/wsimports.bat
+++ mashup/bin/wsimports.bat
@@ -1,7 +1,7 @@
 
-wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/populous-1.0/city?wsdl
-wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/populous-1.0/country?wsdl
-wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/populous-1.0/countrylanguage?wsdl
-wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/populous-1.0/infection?wsdl
-wsimport -verbose -s src/main/java/ -d target/ http://triton.idlemonkeys.net:8080/populous-1.0/language?wsdl
+wsimport -verbose -s src/main/java/ -d target/ http://p.opulo.us:8080/populous-1.0/city?wsdl
+wsimport -verbose -s src/main/java/ -d target/ http://p.opulo.us:8080/populous-1.0/country?wsdl
+wsimport -verbose -s src/main/java/ -d target/ http://p.opulo.us:8080/populous-1.0/countrylanguage?wsdl
+wsimport -verbose -s src/main/java/ -d target/ http://p.opulo.us:8080/populous-1.0/infection?wsdl
+wsimport -verbose -s src/main/java/ -d target/ http://p.opulo.us:8080/populous-1.0/language?wsdl
 wsimport -verbose -s src/main/java/ -d target/ http://www.webservicex.net/geoipservice.asmx?WSDL
