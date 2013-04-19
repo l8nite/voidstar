cf8421f4a8550b2843ec8b0eef3995a72aea58cb
Shaun Guth <shaun.guth@gmail.com>
2013-04-18 22:12:23 -0700

    make epidemic name persist on provider

--- infectious-simulator/src/main/java/us/opulo/p/simulator/modules/random/RandomEventDetailProvider.java
+++ infectious-simulator/src/main/java/us/opulo/p/simulator/modules/random/RandomEventDetailProvider.java
@@ -5,19 +5,26 @@ import java.util.UUID;
 
 import javax.inject.Provider;
 
+import com.google.inject.Singleton;
+
 import us.opulo.p.dao.InfectionEventDetail;
 
+@Singleton
 public class RandomEventDetailProvider implements Provider<InfectionEventDetail> {
-
 	// TODO: move these into random strainprovider, mutationprovider, vectorprovider etc..
 	private final String[] strains = { "Alpha Zero", "Beta One", "Cappa Two" };
 	private final String[] mutations = { "M1", "M2", "M3", "M4", "M5" };
 	private final String[] vectors = { "Airborne", "Contact", "Water" };
+	private final String epidemicName;
+	
+	public RandomEventDetailProvider () {
+		this.epidemicName = UUID.randomUUID().toString();
+	}
 
 	@Override
 	public InfectionEventDetail get() {
 		Random rand = new Random();
-		return new InfectionEventDetail(UUID.randomUUID().toString(), strains[rand.nextInt(strains.length)], mutations[rand.nextInt(mutations.length)], vectors[rand.nextInt(vectors.length)]);
+		return new InfectionEventDetail(epidemicName, strains[rand.nextInt(strains.length)], mutations[rand.nextInt(mutations.length)], vectors[rand.nextInt(vectors.length)]);
 	}
 
 }
