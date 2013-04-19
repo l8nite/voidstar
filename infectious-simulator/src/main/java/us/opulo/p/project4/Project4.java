4842785c5823d460711b2daac61632b6d45fab79
Shaun Guth <shaun.guth@gmail.com>
2013-04-18 23:01:25 -0700

    more queries

--- infectious-simulator/src/main/java/us/opulo/p/project4/Project4.java
+++ infectious-simulator/src/main/java/us/opulo/p/project4/Project4.java
@@ -26,35 +26,41 @@ public class Project4 {
 	*/
 	
 	public static void main(String[] args) {
-		Session session = SessionManager.get();
+//		Session session = SessionManager.get();
 		
 		// Dimensions: Detail, Date, City
 
 		// 1a. Detail + Date
 		// How many infected by Epidemic by Year
-		// SELECT InfectionEventDetail.Epidemic, InfectionEventDate.Year, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN InfectionEventDetail ON InfectionEvent.InfectionEventDetailID = InfectionEventDetail.ID INNER JOIN InfectionEventDate ON InfectionEvent.InfectionEventDateID = InfectionEventDate.ID GROUP BY InfectionEventDetail.Epidemic, InfectionEventDate.Year ORDER BY InfectionEventDate.Year;
+		//   SELECT InfectionEventDetail.Epidemic, InfectionEventDate.Year, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN InfectionEventDetail ON InfectionEvent.InfectionEventDetailID = InfectionEventDetail.ID INNER JOIN InfectionEventDate ON InfectionEvent.InfectionEventDateID = InfectionEventDate.ID GROUP BY InfectionEventDetail.Epidemic, InfectionEventDate.Year ORDER BY InfectionEventDate.Year, InfectionEventDetail.Epidemic;
 		
 		// 1b. City + Detail
 		// How many infected in City by Vector
-		// SELECT CONCAT(City.Name, CONCAT(", ", Country.Name)) as CityCountry, InfectionEventDetail.Vector, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN City ON InfectionEvent.CityID = City.ID INNER JOIN Country ON City.CountryCode = Country.Code INNER JOIN InfectionEventDetail ON InfectionEvent.InfectionEventDetailID = InfectionEventDetail.ID GROUP BY CityCountry, InfectionEventDetail.Vector;
+		//   SELECT CONCAT(City.Name, CONCAT(", ", Country.Name)) as CityCountry, InfectionEventDetail.Vector, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN City ON InfectionEvent.CityID = City.ID INNER JOIN Country ON City.CountryCode = Country.Code INNER JOIN InfectionEventDetail ON InfectionEvent.InfectionEventDetailID = InfectionEventDetail.ID GROUP BY CityCountry, InfectionEventDetail.Vector;
 		
 		// 1c. City + Date
 		// How many infected in City by Year
-		// SELECT CONCAT(City.Name, CONCAT(", ", Country.Name)) as CityCountry, InfectionEventDate.Year, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN City ON InfectionEvent.CityID = City.ID INNER JOIN Country ON City.CountryCode = Country.Code INNER JOIN InfectionEventDate ON InfectionEvent.InfectionEventDateID = InfectionEventDate.ID GROUP BY CityCountry, InfectionEventDate.Year ORDER BY InfectionEventDate.Year;
+		//   SELECT CONCAT(City.Name, CONCAT(", ", Country.Name)) as CityCountry, InfectionEventDate.Year, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN City ON InfectionEvent.CityID = City.ID INNER JOIN Country ON City.CountryCode = Country.Code INNER JOIN InfectionEventDate ON InfectionEvent.InfectionEventDateID = InfectionEventDate.ID GROUP BY CityCountry, InfectionEventDate.Year ORDER BY InfectionEventDate.Year;
 		
-		// Drill up & Down 
-		// Base Report: How many infected by Strain within a single Epidemic by Year
-		// Drill down: How many infected by Mutation within a Strain within a single Epidemic by Year
-		// Drill up: How many infected by Epidemic by Year (re-use from 1a)
+		// 2. Drill up & Down 
+		//
+		// 2a. Base Report: How many infected by Strain within a single Epidemic by Year
+		//   SELECT InfectionEventDetail.Strain, InfectionEventDetail.Epidemic, InfectionEventDate.Year, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN InfectionEventDetail ON InfectionEvent.InfectionEventDetailID = InfectionEventDetail.ID INNER JOIN InfectionEventDate ON InfectionEvent.InfectionEventDateID = InfectionEventDate.ID WHERE InfectionEventDetail.Epidemic = '209efbc0-6900-4b8f-a296-9b1129654722' GROUP BY InfectionEventDetail.Strain, InfectionEventDate.Year ORDER BY InfectionEventDate.Year, InfectionEventDetail.Strain; 
+		// 
+		// 2b. Drill down: How many infected by Mutation within "Alpha Zero" Strain within a single Epidemic by Year
+		//   SELECT InfectionEventDetail.Mutation, InfectionEventDetail.Strain, InfectionEventDetail.Epidemic, InfectionEventDate.Year, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN InfectionEventDetail ON InfectionEvent.InfectionEventDetailID = InfectionEventDetail.ID INNER JOIN InfectionEventDate ON InfectionEvent.InfectionEventDateID = InfectionEventDate.ID WHERE InfectionEventDetail.Epidemic = '209efbc0-6900-4b8f-a296-9b1129654722' AND InfectionEventDetail.Strain = 'Alpha Zero' GROUP BY InfectionEventDetail.Mutation, InfectionEventDate.Year ORDER BY InfectionEventDate.Year, InfectionEventDetail.Mutation;
+		//
+		// 2c. Drill up: How many infected by Epidemic by Year (re-use from 1a)
+		//   SELECT InfectionEventDetail.Epidemic, InfectionEventDate.Year, SUM(InfectionEvent.Infected) FROM InfectionEvent INNER JOIN InfectionEventDetail ON InfectionEvent.InfectionEventDetailID = InfectionEventDetail.ID INNER JOIN InfectionEventDate ON InfectionEvent.InfectionEventDateID = InfectionEventDate.ID GROUP BY InfectionEventDetail.Epidemic, InfectionEventDate.Year ORDER BY InfectionEventDate.Year, InfectionEventDetail.Epidemic;
 		
-		// Slice & Dice
-		// Base report: How many infected by Strain within a single Epidemic by Year
-		// Slice: How many infected by Strain within a single Epidemic in 2013 (sliced by Year)
-		// Dice: How many infected by Strain A or B within a single Epidemic by Years 2000 through 2013
+		// 3. Slice & Dice
+		// 3a. Base report: How many infected by Strain within a single Epidemic by Year
+		// 3b. Slice: How many infected by Strain within a single Epidemic in 2013 (sliced by Year)
+		// 3c. Dice: How many infected by Strain A or B within a single Epidemic by Years 2000 through 2013
 		
-		// Pivot
-		// Normal: Infections by City and Vector (3rd dimension Year)
-		// Pivot: Infections by Vector and Year
+		// 4. Pivot
+		// 4a. Normal: Infections by City and Vector (3rd dimension Year)
+		// 4b. Pivot: Infections by Vector and Year
 	}
 
 }
